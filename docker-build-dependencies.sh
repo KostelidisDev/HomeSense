9be977307c94f59de521e1e157484d14be15f51e
Iordanis Kostelidis <ikostelidis@kostelidis.dev>
2025-02-01 02:21:43 +0200

    change amazon corretto with eclipse temurin (alpine) docker images

--- docker-build-dependencies.sh
+++ docker-build-dependencies.sh
@@ -2,6 +2,6 @@
 
 docker build \
   -f ./docker/Dockerfile.dependencies \
-  --build-arg BASE_BUILDER_IMAGE="maven:3-amazoncorretto-21" \
+  --build-arg BASE_BUILDER_IMAGE="maven:3.9.9-eclipse-temurin-21-alpine" \
   -t homesense-dependencies:latest \
   .
